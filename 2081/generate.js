805342d261aed37c480b3ef0972191b3a8080a94
null <nullpointerexception38@gmail.com>
2025-06-23 21:41:08 +0800

    update

--- 2081/generate.js
+++ 2081/generate.js
@@ -27,7 +27,6 @@ function kMirror() {
     if (nums.length >= 30) {
       return nums
     }
-    let count = 0
     for (const str of strs) {
       const num = parseInt(str, base)
       if (isPalindrome(String(num))) {
@@ -37,7 +36,6 @@ function kMirror() {
         }
       }
     }
-    return count
   }
   const map = {}
   for (let base = 2; base <= 9; base++) {
